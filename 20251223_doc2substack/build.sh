a64aa39a5bf0198fdd1123a043da7a8cf04a7e91
B D <dinunnob@gmail.com>
2025-12-23 16:11:49 -0500

    Remove build.sh from git tracking

--- 20251223_doc2substack/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Convert to HTML using the shared utility
-cd ../utils && uv run doc2substack.py ../20251223_doc2substack/announcement.md --output ../20251223_doc2substack/announcement.html
-
-echo "Build complete. Check announcement.html"
